821f69e7c987c5b699a03a5540fc3b8ec937c1f6
jaestrella <jaestrella1991@gmail.com>
2017-11-23 10:21:46 +0100

    Initial commit

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/material08b/material08b.java
@@ -0,0 +1,13 @@
+package com.iesvirgendelcarmen.dam.material08b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class material08b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material08b);
+    }
+}
